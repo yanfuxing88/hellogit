061d2e31fd35232ac369054399606c82f0a0b9a7
yanfuxing88 <1075081786@qq.com>
2020-11-09 17:37:02 +0800

    添加文件

---
+++ HelloGit/HelloGit/src/main/java/com/atguigu/git/HelloGit.java
@@ -0,0 +1,12 @@
+package java.com.atguigu.git;
+
+/**
+ * @author yan
+ * @create 2020-11-09 17:24
+ */
+public class HelloGit {
+
+    public static void main(String[] args) {
+        System.out.println("你好");
+    }
+}
